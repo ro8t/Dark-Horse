f278c837aac38c5923a395e991fe8b0cdb2c814e
Matt <morrow2012matthew@gmail.com>
2019-09-30 19:44:21 -0700

    Add files via upload

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ nyc_track_length = 660.75
 bart_track_length = 149.3
 sp_track_length = 60.1
 london_track_length = 250
-nyc_avg_fare = 2.75
 
 # Land coverages
 nyc_land_cov = 302.643
@@ -12,4 +11,7 @@ sp_land_cov = 587
 london_land_cov = 607
 
 # Fares
-# nyc_avg_fare = 2.75
+nyc_avg_fare = 2.75
+
+# Opperating Budget
+nyc_budg = 16000000000
