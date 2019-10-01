8ee683b7a7cbdbb9e43feaed9c5f5b030012dc77
Rohan Bhanot <rohanbhanot@Rohans-MacBook-Pro.local>
2019-09-30 20:48:48 -0700

    manual merge

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ nyc_track_length = 660.75
 bart_track_length = 149.3
 sp_track_length = 60.1
 london_track_length = 250
-
 # Land coverages
 nyc_land_cov = 302.643
 bart_land_cov = 494.684
 sp_land_cov = 587
 london_land_cov = 607
+# Fares
+nyc_avg_fare = 2.75
+# Opperating Budget
+nyc_budg = 16000000000
